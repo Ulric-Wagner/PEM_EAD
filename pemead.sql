ea14938003ad729ec81aeec415002880e177651a
Ulric-Wagner <ulric.wagner@gmail.com>
2023-04-06 01:03:05 +0200

    base de donnée vierge

--- pemead.sql
+++ pemead.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 05 avr. 2023 à 22:11
+-- Généré le : mer. 05 avr. 2023 à 23:02
 -- Version du serveur : 5.7.36
 -- Version de PHP : 8.0.13
 
@@ -33,14 +33,7 @@ CREATE TABLE IF NOT EXISTS `administrateurs` (
   `UID` int(255) DEFAULT NULL,
   PRIMARY KEY (`AID`),
   UNIQUE KEY `UID` (`UID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
-
---
--- Déchargement des données de la table `administrateurs`
---
-
-INSERT INTO `administrateurs` (`AID`, `UID`) VALUES
-(2, 1);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
 -- --------------------------------------------------------
 
@@ -57,27 +50,7 @@ CREATE TABLE IF NOT EXISTS `copies` (
   `Path` varchar(255) COLLATE latin1_general_cs DEFAULT NULL,
   `Date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`ERID`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
-
---
--- Déchargement des données de la table `copies`
---
-
-INSERT INTO `copies` (`ERID`, `EAID`, `UID`, `Note`, `Path`, `Date`) VALUES
-(10, 2, 2, NULL, NULL, '2023-04-05 23:50:07'),
-(11, 2, 2, NULL, NULL, '2023-04-05 23:50:14'),
-(12, 2, 2, NULL, NULL, '2023-04-05 23:50:32'),
-(13, 2, 2, NULL, NULL, '2023-04-05 23:52:38'),
-(14, 2, 2, NULL, NULL, '2023-04-05 23:52:46'),
-(15, 2, 2, NULL, NULL, '2023-04-05 23:54:15'),
-(16, 2, 2, NULL, NULL, '2023-04-05 23:54:18'),
-(17, 2, 2, NULL, NULL, '2023-04-05 23:54:32'),
-(18, 2, 2, NULL, NULL, '2023-04-05 23:58:51'),
-(19, 2, 2, NULL, NULL, '2023-04-05 23:58:58'),
-(20, 2, 2, NULL, NULL, '2023-04-06 00:01:10'),
-(21, 2, 2, NULL, NULL, '2023-04-06 00:01:56'),
-(22, 2, 2, NULL, NULL, '2023-04-06 00:06:49'),
-(23, 2, 2, NULL, NULL, '2023-04-06 00:07:13');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
 -- --------------------------------------------------------
 
@@ -92,17 +65,7 @@ CREATE TABLE IF NOT EXISTS `cours` (
   `GID` int(255) DEFAULT NULL,
   PRIMARY KEY (`CID`),
   UNIQUE KEY `Cours` (`Cours`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
-
---
--- Déchargement des données de la table `cours`
---
-
-INSERT INTO `cours` (`CID`, `Cours`, `GID`) VALUES
-(1, 'CSUP CYBER', 1),
-(2, 'BAT SYNUM', 1),
-(3, 'BS OPS', 2),
-(4, 'CSUP TECHOPS', 2);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
 -- --------------------------------------------------------
 
@@ -119,14 +82,7 @@ CREATE TABLE IF NOT EXISTS `documents` (
   `FID` int(255) DEFAULT NULL,
   `statut` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`DID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
-
---
--- Déchargement des données de la table `documents`
---
-
-INSERT INTO `documents` (`DID`, `Document`, `Description`, `MID`, `FID`, `statut`) VALUES
-(1, 'Pare-feu', 'il s&amp;#039agit d&amp;#039un cours sur les pare-feu', 1, 1, 1);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
 -- --------------------------------------------------------
 
@@ -141,14 +97,7 @@ CREATE TABLE IF NOT EXISTS `evals` (
   `PID` int(255) DEFAULT NULL,
   `Statut` tinyint(4) DEFAULT '0',
   PRIMARY KEY (`EAID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
-
---
--- Déchargement des données de la table `evals`
---
-
-INSERT INTO `evals` (`EAID`, `ETID`, `PID`, `Statut`) VALUES
-(2, 16, 1, 1);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
 -- --------------------------------------------------------
 
@@ -164,13 +113,6 @@ CREATE TABLE IF NOT EXISTS `evaltemplates` (
   `MID` int(255) DEFAULT NULL,
   PRIMARY KEY (`ETID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
-
---
--- Déchargement des données de la table `evaltemplates`
---
-
-INSERT INTO `evaltemplates` (`ETID`, `Eval`, `Description`, `MID`) VALUES
-(16, 'Test de bon fonctionement', 'petit devoir', 1);
 
 -- --------------------------------------------------------
 
@@ -190,13 +132,6 @@ CREATE TABLE IF NOT EXISTS `fichiers` (
   UNIQUE KEY `Path` (`Path`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
---
--- Déchargement des données de la table `fichiers`
---
-
-INSERT INTO `fichiers` (`FID`, `Fichier`, `Type`, `GID`, `Path`, `Poster`) VALUES
-(1, 'RSEC_FW.pptx', 'pptx', 2, 'files/RSEC_FW.pptx', 'M. BAH Alfa');
-
 -- --------------------------------------------------------
 
 --
@@ -210,14 +145,6 @@ CREATE TABLE IF NOT EXISTS `groupements` (
   PRIMARY KEY (`GID`),
   UNIQUE KEY `Groupement` (`Groupement`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
-
---
--- Déchargement des données de la table `groupements`
---
-
-INSERT INTO `groupements` (`GID`, `Groupement`) VALUES
-(2, 'OPS'),
-(1, 'SIC');
 
 -- --------------------------------------------------------
 
@@ -233,18 +160,6 @@ CREATE TABLE IF NOT EXISTS `instructeurs` (
   PRIMARY KEY (`IID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
---
--- Déchargement des données de la table `instructeurs`
---
-
-INSERT INTO `instructeurs` (`IID`, `UID`, `GID`) VALUES
-(23, 1, 1),
-(28, 3, 1),
-(29, 3, 1),
-(30, 3, 2),
-(31, 3, 1),
-(32, 3, 2);
-
 -- --------------------------------------------------------
 
 --
@@ -259,14 +174,6 @@ CREATE TABLE IF NOT EXISTS `matieres` (
   PRIMARY KEY (`MID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
---
--- Déchargement des données de la table `matieres`
---
-
-INSERT INTO `matieres` (`MID`, `Matiere`, `CID`) VALUES
-(1, 'PARE-FEU', 1),
-(2, 'LINUX', 4);
-
 -- --------------------------------------------------------
 
 --
@@ -280,13 +187,6 @@ CREATE TABLE IF NOT EXISTS `pilotes` (
   `CID` int(255) DEFAULT NULL,
   PRIMARY KEY (`PILID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
-
---
--- Déchargement des données de la table `pilotes`
---
-
-INSERT INTO `pilotes` (`PILID`, `UID`, `CID`) VALUES
-(19, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -303,15 +203,6 @@ CREATE TABLE IF NOT EXISTS `promotions` (
   UNIQUE KEY `CID` (`CID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
---
--- Déchargement des données de la table `promotions`
---
-
-INSERT INTO `promotions` (`PID`, `CID`, `Promotion`) VALUES
-(1, 1, '22.1'),
-(2, 2, '23.1'),
-(3, 4, '22.1');
-
 -- --------------------------------------------------------
 
 --
@@ -326,14 +217,6 @@ CREATE TABLE IF NOT EXISTS `questions` (
   PRIMARY KEY (`QID`),
   KEY `QETID` (`ETID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
-
---
--- Déchargement des données de la table `questions`
---
-
-INSERT INTO `questions` (`QID`, `ETID`, `Question`) VALUES
-(2, 16, 'quel est le role d&amp;#039un pare-feu?'),
-(3, 16, 'dans cette liste cochez les pare-feu:');
 
 -- --------------------------------------------------------
 
@@ -351,17 +234,6 @@ CREATE TABLE IF NOT EXISTS `reponses` (
   KEY `QQID` (`QID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
---
--- Déchargement des données de la table `reponses`
---
-
-INSERT INTO `reponses` (`RID`, `QID`, `Reponse`, `Points`) VALUES
-(13, 2, 'un puissant sort de mage', -0.5),
-(14, 2, 's&amp;#039interfacer en coupure entre deux réseau afin de filtrer le trafic', 1),
-(15, 2, 'filtrer des url', -0.5),
-(17, 3, 'ALCAZAR', -0.5),
-(18, 3, 'PFSENSE', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -375,13 +247,6 @@ CREATE TABLE IF NOT EXISTS `students` (
   `PID` int(255) DEFAULT NULL,
   PRIMARY KEY (`SID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
-
---
--- Déchargement des données de la table `students`
---
-
-INSERT INTO `students` (`SID`, `UID`, `PID`) VALUES
-(20, 2, 3);
 
 -- --------------------------------------------------------
 
@@ -403,7 +268,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`UID`),
   UNIQUE KEY `Mail` (`Mail`),
   UNIQUE KEY `Matricule` (`Matricule`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
 --
 -- Contraintes pour les tables déchargées
